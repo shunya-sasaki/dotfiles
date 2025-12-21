9d456a0910bbbb2e5397d3fbc68176b0b7761697
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-12-21 13:46:14 +0900

    feat(wezterm): add Moralerspace font and ss10
    - Add Moralerspace Neon HW to font fallback
    - Add ss10 font feature and reformat configuration

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -10,12 +10,25 @@ config.initial_rows = 30
 config.font = wezterm.font_with_fallback({
 	"Monaspace Neon",
 	"Symbols Nerd Font Mono",
+	"Moralerspace Neon HW",
 	"FiraCode Nerd Font",
 	"IBM Plex Sans JP",
 	"Apple Color Emoji",
 })
-config.harfbuzz_features =
-	{ "calt=1", "liga=1", "ss01=1", "ss02=1", "ss03=1", "ss04=1", "ss05=1", "ss06=1", "ss07=1", "ss08=1", "ss09=1" }
+config.harfbuzz_features = {
+	"calt=1",
+	"liga=1",
+	"ss01=1",
+	"ss02=1",
+	"ss03=1",
+	"ss04=1",
+	"ss05=1",
+	"ss06=1",
+	"ss07=1",
+	"ss08=1",
+	"ss09=1",
+	"ss10=1",
+}
 
 config.enable_kitty_graphics = true
 config.enable_kitty_keyboard = true
