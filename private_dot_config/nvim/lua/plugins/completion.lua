a95368205a94af21d902cf64d1bb3da6b0620e5f
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-12-26 20:31:35 +0900

    build(completion): remove copilot-cmp dependency

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -7,7 +7,6 @@ return {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
-			"zbirnenbaum/copilot-cmp",
 			"saadparwaiz1/cmp_luasnip",
 			"petertriho/cmp-git",
 			"onsails/lspkind-nvim",
