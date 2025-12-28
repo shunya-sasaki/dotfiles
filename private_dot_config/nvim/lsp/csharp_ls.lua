13bd168bdb0d6e71f47b99e090ecaced806c4433
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-12-28 14:33:45 +0900

    feat(lsp): add bashls, stylua and update csharp_ls
    - Add configuration for bash-language-server.
    - Add configuration for stylua as an LSP.
    - Update csharp-ls with improved root detection and RPC start.

--- private_dot_config/nvim/lsp/csharp_ls.lua
+++ private_dot_config/nvim/lsp/csharp_ls.lua
@@ -1,4 +1,21 @@
 return {
-		cmd = { "csharp-ls" },
-		filetypes = { "cs" },
+	cmd = function(dispatchers, config)
+		return vim.lsp.rpc.start({ "csharp-ls" }, dispatchers, {
+			cwd = config.cmd_cwd or config.root_dir,
+			env = config.cmd_env,
+			detached = config.detached,
+		})
+	end,
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		on_dir(
+			util.root_pattern("*.sln")(fname)
+				or util.root_pattern("*.slnx")(fname)
+				or util.root_pattern("*.csproj")(fname)
+		)
+	end,
+	filetypes = { "cs" },
+	init_options = {
+		AutomaticWorkspaceInit = true,
+	},
 }
