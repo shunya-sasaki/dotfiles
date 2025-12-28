59c5d80193d54211ac99783e27f2c698ae9681ca
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-12-28 17:17:24 +0900

    refactor(lsp): update powershell_es configuration
    Update the PowerShell Editor Services configuration to use a custom
    start function with configurable bundle paths and session details.

--- private_dot_config/nvim/lsp/powershell_es.lua
+++ private_dot_config/nvim/lsp/powershell_es.lua
@@ -1,7 +1,19 @@
+vim.lsp.config("powershell_es", { bundle_path = vim.env.HOME .. "/.pwsh/module" })
+
 return {
-		cmd = {
-			vim.fn.stdpath("data")
-				.. "/mason/packages/powershell-editor-services/PowerShellEditorServices/Start-EditorServices.ps1",
-		},
-		filetypes = { "ps1" },
-	}
+	cmd = function(dispatchers)
+		local temp_path = vim.fn.stdpath("cache")
+		local bundle_path = vim.lsp.config.powershell_es.bundle_path
+
+		local shell = vim.lsp.config.powershell_es.shell or "pwsh"
+
+		local command_fmt =
+			[[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
+		local command = command_fmt:format(bundle_path, bundle_path, temp_path, temp_path)
+		local cmd = { shell, "-NoLogo", "-NoProfile", "-Command", command }
+
+		return vim.lsp.rpc.start(cmd, dispatchers)
+	end,
+	filetypes = { "ps1" },
+	root_markers = { "PSScriptAnalyzerSettings.psd1", ".git" },
+}
