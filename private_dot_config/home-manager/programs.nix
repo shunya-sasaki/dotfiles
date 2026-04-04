d51030f4927d342a3b5914a5702ba6e66ee6948c
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-04 19:12:14 +0900

    feat(vim): add vim with commentary and lsp plugins

--- private_dot_config/home-manager/programs.nix
+++ private_dot_config/home-manager/programs.nix
@@ -1,8 +1,34 @@
 { pkgs, ... }:
 
+let
+    vimPlug = pkgs.vimUtils.buildVimPlugin;
+in
 {
   home-manager.enable = true;
   zsh.enable = false;
+  vim = {
+    enable = true;
+    plugins = [
+      (vimPlug {
+        name = "vim-commentary";
+        src = pkgs.fetchFromGitHub {
+          owner = "tpope";
+          repo = "vim-commentary";
+          rev = "v1.3";
+          sha256 = "sha256-pmADOvvMoTlJctAsfZQVuYlj7L8nk67EFvZKy9QV9Qs=";
+        };
+      })
+      (vimPlug {
+        name = "lsp";
+        src = pkgs.fetchFromGitHub {
+          owner = "yegappan";
+          repo = "lsp";
+          rev = "main";
+          sha256 = "sha256-LBB77pQKKVTKCwfbIcuxDAwFsyRdcnzbtcwLxERWvzU=";
+        };
+      })
+    ];
+  };
   direnv = {
     enable = true;
     nix-direnv.enable = true;
