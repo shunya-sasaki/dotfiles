d1711d91cbfb2c3a156a64504915bca110a8046a
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-05 19:28:30 +0900

    feat(vim): add plugins and external vimrc config

--- private_dot_config/home-manager/programs.nix
+++ private_dot_config/home-manager/programs.nix
@@ -8,6 +8,7 @@ in
   zsh.enable = false;
   vim = {
     enable = true;
+    extraConfig = "source ~/.config/vim/vimrc";
     plugins = [
       (vimPlug {
         name = "vim-commentary";
@@ -19,12 +20,97 @@ in
         };
       })
       (vimPlug {
+        name = "vim-easymotion";
+        src = pkgs.fetchFromGitHub {
+          owner = "easymotion";
+          repo = "vim-easymotion";
+          rev = "v3.0.1";
+          sha256 = "sha256-yX5O7DUrIiPUpaXDli/S6hucsQckeyi2U26YLUNOth8=";
+        };
+      })
+      # Interface -------------------------------------------------------------
+      (vimPlug {
+        name = "vim-airline";
+        src = pkgs.fetchFromGitHub {
+          owner = "vim-airline";
+          repo = "vim-airline";
+          rev = "v0.11";
+          sha256 = "sha256-8zSK98AHfXo247Efm0XxkzQxkfjN3XKY3xcRM06ueqo=";
+        };
+      })
+      (vimPlug {
+        name = "vim-airline-themes";
+        src = pkgs.fetchFromGitHub {
+          owner = "vim-airline";
+          repo = "vim-airline-themes";
+          rev = "master";
+          sha256 = "sha256-xTgitX/kL8m/zjcxjCe4WWvhKfVPS284GoZjWkWc/gY=";
+        };
+      })
+      (vimPlug {
+        name = "vim-devicons";
+        src = pkgs.fetchFromGitHub {
+          owner = "ryanoasis";
+          repo = "vim-devicons";
+          rev = "master";
+          sha256 = "sha256-9g3UdBcc34MR/BrKYqOki2Ge29k/QaRMsnyY+Pq9UE8=";
+        };
+      })
+
+
+
+      (vimPlug {
+        name = "tokyonight-vim";
+        src = pkgs.fetchFromGitHub {
+          owner = "ghifarit53";
+          repo = "tokyonight-vim";
+          rev = "master";
+          sha256 = "sha256-ui/6xv8PH6KuQ4hG1FNMf6EUdF2wfWPNgG/GMXYvn/8=";
+        };
+      })
+      (vimPlug {
+        name = "fzf";
+        src = pkgs.fetchFromGitHub {
+          owner = "junegunn";
+          repo = "fzf";
+          rev = "master";
+          sha256 = "sha256-fEHNE51VTggSwNutefCg/WueaXRE1J0D/dyVPFqhTJg=";
+        };
+      })
+      (vimPlug {
+        name = "fzf.vim";
+        src = pkgs.fetchFromGitHub {
+          owner = "junegunn";
+          repo = "fzf.vim";
+          rev = "master";
+          sha256 = "sha256-n0jPX5keDf9qUuqyMKosLoI42DRerky9N5moFuuPccY=";
+        };
+      })
+      (vimPlug {
         name = "lsp";
         src = pkgs.fetchFromGitHub {
           owner = "yegappan";
           repo = "lsp";
           rev = "main";
           sha256 = "sha256-LBB77pQKKVTKCwfbIcuxDAwFsyRdcnzbtcwLxERWvzU=";
+        };
+      })
+      (vimPlug {
+        name = "vim-polyglot";
+        src = pkgs.fetchFromGitHub {
+          owner = "sheerun";
+        repo = "vim-polyglot";
+          rev = "v4.17.1";
+          sha256 = "sha256-UkCKWLHh2KCK//DI62ZCyJjwMAnXRwzfzKZjFPBWQro=";
+        };
+      })
+      (vimPlug {
+        name = "vim-floaterm";
+        src = pkgs.fetchFromGitHub {
+        owner = "voldikss";
+        repo = "vim-floaterm";
+          rev = "master";
+          sha256 = "sha256-A4SpMFB1thUhF4N5I7N+HnycE/OXQQbJVMr4wo0gKUc=";
         };
       })
     ];
