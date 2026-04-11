d32b7c9a30fd2b46b955fb631c90bc7ef8d7c231
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-11 15:29:40 +0900

    feat(nvim): configure pyright and disable ruff hover

--- private_dot_config/nvim/after/lsp/pyright.lua
+++ private_dot_config/nvim/after/lsp/pyright.lua
@@ -2,9 +2,15 @@ return {
 	cmd = { "pyright-langserver", "--stdio" },
 	filetypes = { "python" },
 	settings = {
+		pyright = {
+			disableOrganizeImports = true,
+		},
 		python = {
 			venvPath = ".",
 			venv = ".venv",
+			analysis = {
+				ignore = {'*'}
+			}
 		},
 	},
 }
--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -40,6 +40,20 @@ return {
 			vim.lsp.enable("powershell_es")
 			-- Nix
 			vim.lsp.enable("nixd")
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client == nil then
+						return
+					end
+					if client.name == "ruff" then
+						-- Disable hover in favor of Pyright
+						client.server_capabilities.hoverProvider = false
+					end
+				end,
+				desc = "LSP: Disable hover capability from Ruff",
+			})
 		end,
 	},
 	{
