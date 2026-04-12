3b206a104dba91674c11fa108ad45b59d8a1b4c0
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-12 16:32:20 +0900

    feat(yazi): add mime type mappings for more file types

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -2,14 +2,23 @@ require("mime-ext.local"):setup({
 	-- Expand the existing filename database (lowercase), for example:
 	with_files = {
 		makefile = "text/makefile",
+		vimrc = "text/vim",
+    config = "text/config",
 		["requirements.txt"] = "text/requirements",
+    [".gitattributes"] = "text/gitattributes",
+    ["dot_zshrc"] = "text/zsh",
+    ["dot_bashrc"] = "text/bash",
+    [".chezmoiignore"] = "text/chezmoiignore",
 	},
 
 	-- Expand the existing extension database (lowercase), for example:
 	with_exts = {
 		mk = "text/makefile",
 		ps1 = "text/x-powershell",
+    vim = "text/vim",
+    kdl = "text/kdl",
 		["code-snippets"] = "text/code-snippets",
+    ["json.tmpl"] = "text/json"
 	},
 
 	-- If the MIME type is not in both filename and extension databases,
