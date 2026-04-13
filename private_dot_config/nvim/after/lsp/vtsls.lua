8e43a51ed65fbd5d050daa648ab5597b1a397cfe
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-13 21:46:04 +0900

    fix(lsp): disable formatting in vtsls

--- private_dot_config/nvim/after/lsp/vtsls.lua
+++ private_dot_config/nvim/after/lsp/vtsls.lua
@@ -1,4 +1,20 @@
 return {
   cmd = { "vtsls", "--stdio" },
   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
+  settings = {
+    typescript = {
+      format = {
+        enable = false,
+      },
+    },
+    javascript = {
+      format = {
+        enable = false,
+      },
+    },
+  },
 }
