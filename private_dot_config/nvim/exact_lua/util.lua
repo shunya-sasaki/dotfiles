617c90d08d46ab1c4b0b8f7d0cbbb38ca48484bd
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-17 08:34:04 +0900

    refactor(nvim): migrate lua dirs to exact_ prefix for chezmoi

---
+++ private_dot_config/nvim/exact_lua/util.lua
@@ -0,0 +1,32 @@
+local function get_os_name()
+	if vim.fn.has("mac") == 1 then
+		return "mac"
+	elseif vim.fn.has("linux") == 1 then
+		return "linux"
+	elseif vim.fn.has("unix") == 1 then
+		return "unix"
+	elseif vim.fn.has("sun") == 1 then
+		return "sun"
+	elseif vim.fn.has("bsd") == 1 then
+		return "bsd"
+	elseif vim.fn.has("win32") == 1 then
+		return "win32"
+	elseif vim.fn.has("win64") == 1 then
+		return "win64"
+	elseif vim.fn.has("wsl") == 1 then
+		return "wsl"
+	end
+end
+
+local function get_nvim_agent()
+	local agent = os.getenv("NVIM_AGENT")
+	if not agent or agent == "" then
+		return "gemini"
+	end
+	return agent
+end
+
+return {
+	get_os_name = get_os_name,
+	get_nvim_agent = get_nvim_agent,
+}
