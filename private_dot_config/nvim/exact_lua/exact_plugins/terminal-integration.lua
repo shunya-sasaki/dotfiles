226fb4b8f618e75e8716a899c8acd61b5c97f486
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-17 16:49:41 +0900

    refactor(nvim): rename util module to utils

--- private_dot_config/nvim/exact_lua/exact_plugins/terminal-integration.lua
+++ private_dot_config/nvim/exact_lua/exact_plugins/terminal-integration.lua
@@ -1,55 +1,50 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("toggleterm").setup({
-				direction = "float",
-				float_opts = {
-					width = vim.o.columns,
-					height = vim.o.lines,
-				},
-			})
-			local Terminal = require("toggleterm.terminal").Terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        direction = "float",
+        float_opts = {
+          width = vim.o.columns,
+          height = vim.o.lines,
+        },
+      })
+      local Terminal = require("toggleterm.terminal").Terminal
 
-			local term = Terminal:new({
-				hidden = true,
-				direction = "float",
-				count = 1,
-			})
+      local term = Terminal:new({
+        hidden = true,
+        direction = "float",
+        count = 1,
+      })
 
-			local lazygit = Terminal:new({
-				cmd = "lazygit",
-				hidden = true,
-				direction = "float",
-				count = 2,
-			})
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+        count = 2,
+      })
 
-			local agent = Terminal:new({
-				cmd = require("util").get_nvim_agent(),
-				hidden = true,
-				direction = "float",
-				count = 3,
-			})
+      local agent = Terminal:new({
+        cmd = require("utils").get_nvim_agent(),
+        hidden = true,
+        direction = "float",
+        count = 3,
+      })
 
-			function Lazygit_toggle()
-				lazygit:toggle()
-			end
+      function Lazygit_toggle()
+        lazygit:toggle()
+      end
 
-			function Agent_toggle()
-				agent:toggle()
-			end
+      function Agent_toggle()
+        agent:toggle()
+      end
 
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>g",
-				"<cmd>lua Lazygit_toggle()<CR>",
-				{ noremap = true, silent = true }
-			)
+      vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua Lazygit_toggle()<CR>", { noremap = true, silent = true })
 
-			vim.api.nvim_set_keymap("n", "<leader>i", "<cmd>lua Agent_toggle()<CR>", { noremap = true, silent = true })
-			-- vim.api.nvim_set_keymap("n", "<leader>t", ": ToggleTerm<CR>", { silent = true, noremap = true })
-			vim.api.nvim_set_keymap("n", "<C-t>", ": ToggleTerm<CR>", { silent = true, noremap = true })
-		end,
-	},
+      vim.api.nvim_set_keymap("n", "<leader>i", "<cmd>lua Agent_toggle()<CR>", { noremap = true, silent = true })
+      -- vim.api.nvim_set_keymap("n", "<leader>t", ": ToggleTerm<CR>", { silent = true, noremap = true })
+      vim.api.nvim_set_keymap("n", "<C-t>", ": ToggleTerm<CR>", { silent = true, noremap = true })
+    end,
+  },
 }
