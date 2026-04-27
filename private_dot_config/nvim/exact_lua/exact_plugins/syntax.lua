7e438ef476a5516c4950534eb73868fe56d395f4
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-27 20:17:39 +0900

    feat(nvim): add vue treesitter parser and reformat indentation

--- private_dot_config/nvim/exact_lua/exact_plugins/syntax.lua
+++ private_dot_config/nvim/exact_lua/exact_plugins/syntax.lua
@@ -1,66 +1,67 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		branch = "main",
-		build = ":TSUpdate",
-		config = function()
-			ts = require("nvim-treesitter")
-			ts.setup({
-				install_dir = vim.fn.stdpath("data") .. "/site",
-			})
-			languages = {
-				"bash",
-				"c",
-				"c_sharp",
-				"cmake",
-				"css",
-				"diff",
-				"doxygen",
-				"fortran",
-				"git_config",
-				"gitcommit",
-				"gitignore",
-				"haskell",
-				"html",
-				"htmldjango",
-				"http",
-				"ini",
-				"javascript",
-				"jsdoc",
-				"json",
-				"kdl",
-				"lua",
-				"luadoc",
-				"make",
-				"markdown",
-				"markdown_inline",
-				"mermaid",
-				"nginx",
-				"nix",
-				"powershell",
-				"python",
-				"razor",
-				"requirements",
-				"rust",
-				"sql",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"yaml",
-			}
-			ts.install(languages)
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = languages,
-				callback = function()
-					vim.treesitter.start()
-				end,
-			})
-			vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-			vim.wo[0][0].foldmethod = "expr"
-			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+      ts = require("nvim-treesitter")
+      ts.setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
+      languages = {
+        "bash",
+        "c",
+        "c_sharp",
+        "cmake",
+        "css",
+        "diff",
+        "doxygen",
+        "fortran",
+        "git_config",
+        "gitcommit",
+        "gitignore",
+        "haskell",
+        "html",
+        "htmldjango",
+        "http",
+        "ini",
+        "javascript",
+        "jsdoc",
+        "json",
+        "kdl",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "mermaid",
+        "nginx",
+        "nix",
+        "powershell",
+        "python",
+        "razor",
+        "requirements",
+        "rust",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "vue",
+        "yaml",
+      }
+      ts.install(languages)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = languages,
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+      vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.wo[0][0].foldmethod = "expr"
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  },
 }
